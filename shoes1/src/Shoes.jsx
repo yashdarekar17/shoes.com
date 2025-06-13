a8abc417ddedf8aee70d5055183bba4bd18c33c4
yashdarekar17 <dyash8608@gmail.com>
2025-06-13 10:53:34 -0200

    Update Shoes.jsx

--- shoes1/src/Shoes.jsx
+++ shoes1/src/Shoes.jsx
@@ -1,6 +1,6 @@
 import Nikep from "./Nikep.png"
 import Puma2 from "./Puma2.png"
-import Addidas2 from "./addidas3.png"
+import ad from"./ad.png";
 import crocs from "./Crocs.png"
 import S1 from "./S1.png"
 import { Link } from "react-router-dom"
@@ -13,7 +13,7 @@ function Shoes(){
             <h2 className="text-center text-4xl font-bold py-0.5">Brands</h2>
             <ul className="flex justify-between">
                 <li className="w-20"><Link to="/Nikeshoes"><img src={Nikep} alt="" /></Link></li>
-                <li className="w-20"><Link to="/Addias"><img src={Addidas2} alt="" /></Link></li>
+                <li className="w-20"><Link to="/Addias"><img src={ad} alt="" /></Link></li>
                 <li className="w-20"><Link to="/puma"><img src={Puma2} alt="" /></Link></li>
                 <li className="w-20"><Link to="/Crocs"><img src={crocs} alt="" /></Link></li>
             </ul>
